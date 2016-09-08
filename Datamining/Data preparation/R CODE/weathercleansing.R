ed0059a5a8219337d41913192868bce812e67e06
skaul <sonalkaulkar@gmail.com>
2016-09-08 18:22:42 -0400

    deleted old files

--- Datamining/Data preparation/R CODE/weathercleansing.R
+++
@@ -1,4 +0,0 @@
-weatherdata <- read.csv('weathersharkattackintegrated.csv', header = T, stringsAsFactors = F)
-weatherdata $DailyPrecipitation <- as.factor(weatherdata $DailyPrecipitation)
-summary(weatherdata $DailyPrecipitation)
-weatherdata $DailyPrecipitation[weatherdata $DailyPrecipitation == "T"] <-  0
