9e87e53a63c2128b5b902c4ef131908edc0acb2a
sonalkaulkar <sonalkaulkar1@gmail.com>
2016-09-09 16:08:44 -0400

    Added comment

--- DataMining/RCode/discretize_alldatessharkfile.R
+++ DataMining/RCode/discretize_alldatessharkfile.R
@@ -1,3 +1,5 @@
+#Discretization of  additional variables shark attack file with attack = YES
+
 sharkattackoutput <- read.csv("attackdateshark.csv", header = T, stringsAsFactors = F)
 #discretization of dissolved oxygen
 dis_disso2 <-discretize(sharkattackoutput$mod_dissolvedo2,"equalwidth", 3)
