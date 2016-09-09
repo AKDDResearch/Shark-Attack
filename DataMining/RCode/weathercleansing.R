0d9f18d2cf0fea023a499705676a8d83de171c90
sonalkaulkar <sonalkaulkar1@gmail.com>
2016-09-09 16:29:35 -0400

    Added comment

--- DataMining/RCode/weathercleansing.R
+++ DataMining/RCode/weathercleansing.R
@@ -1,3 +1,5 @@
+#Cleanse the weather data precipitation variable to replace 'T' values with zero
+
 weatherdata <- read.csv('weathersharkattackintegrated.csv', header = T, stringsAsFactors = F)
 weatherdata $DailyPrecipitation <- as.factor(weatherdata $DailyPrecipitation)
 summary(weatherdata $DailyPrecipitation)
